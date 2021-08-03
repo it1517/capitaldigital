323b5afda6e2cfb3052be008d68afcc77733b4d2
Itzel Quezada <atl024.itzel@gmail.com>
2021-08-03 06:55:22 -0500

    Initial commit

--- public/main.js
+++ public/main.js
@@ -1,10 +1,19 @@
 // Buttons functions
 let profileBtn = document.querySelector('#profileBtn');
-let carritpBtn = document.querySelector('#carritoBtn');
+let carritoBtn = document.querySelector('#carritoBtn');
+let mobileMenu = document.querySelector('#sidebarMobile');
+let mobileMenu2 = document.querySelector('#sidebarMobile2');
 
 profileBtn.onclick = function() {
   let profileDiv = document.querySelector('#profileDiv');
   showHide(profileDiv);
+}
+
+sidebarBtnCel.onclick = function() {
+  let mobileMenu = document.querySelector('#sidebarMobile');
+  showHide(sidebarMobile);
+  let mobileMenu2 = document.querySelector('#sidebarMobile2');
+  showHide(sidebarMobile2);
 }
 
 carritoBtn.onclick = function() {
@@ -29,6 +38,7 @@ sidebarBtn.onclick = function() {
 
   let sidebar = document.querySelector('aside');
   let sidebarMobile = document.querySelector('#sidebarMobile');
+  let sidebarMobile2 = document.querySelector('#sidebarMobile2');
 
   if(sidebar.classList.contains('sm:block')) {
     sidebar.classList.replace('sm:block', 'sm:hidden');
@@ -43,7 +53,19 @@ sidebarBtn.onclick = function() {
   } else {
     sidebarMobile.classList.add('hidden');
   }
+
+
+  if(sidebarMobile2.classList.contains('hidden')) {
+    sidebarMobile2.classList.remove('hidden');
+
+  } else {
+    sidebarMobile2.classList.add('hidden');
+  }
 }
+
+
+
+
 
 
 
@@ -51,10 +73,12 @@ sidebarBtn.onclick = function() {
 function setColor(color) {
   let sidebar = document.querySelector('aside');
   let sidebarMobile = document.querySelector('#sidebarMobile');
+  let sidebarMobile2 = document.querySelector('#sidebarMobile2');
 
   localStorage.setItem('color', color);
   sidebar.className = color + ' relative w-72 md:w-64 hidden sm:block shadow-xl';
-  sidebarMobile.className = color + ' w-full py-5 px-6 hidden sm:hidden';
+  sidebarMobile.className = color + ' w-full py-5 px-6 hidden sm:hidden max-w-full	';
+  sidebarMobile2.className = color + ' w-full py-5 px-6 hidden sm:hidden max-w-full	';
 }
 
 if(localStorage.getItem('color') === 'null'){
